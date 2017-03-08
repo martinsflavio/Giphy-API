db8c10bee1383d30ea497f4efd51edc4988ebbd1
Flavio dos Reis Martins <flaviodosreismartins@gmail.com>
2017-03-08 15:03:14 -0800

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,28 @@
+	var topics = [];
 
-var topics = [];
-
-var $$ = {
-	submitBtn: $("#btn-submit"),
-	clearBtn: $("#btn-clear"),
-	topicBtn: $(".topic-btn"),
-	/*Elements*/
-	btnBox: $("#btn-box"),
-	gifBox: $("#gif-box"),
-	search: $("#search-input"),
-	selectedTopic: $("#selected-topic")
-}
+	var $$ = {
+		/*Buttons*/
+		submitBtn: $("#btn-submit"),
+		clearBtn: $("#btn-clear"),
+		
+		/*HTML-Elements*/
+		btnBox: $("#btn-box"),
+		gifBox: $("#gif-box"),
+		search: $("#search-input"),
+		selectedTopic: $("#selected-topic")
+	}
 
 $(document).ready(function(){
+
+
+
+	/*=============== Click Events ===============*/
+
 	// Clear Button
 	$$.clearBtn.on("click", function(){
 		$$.gifBox.empty();
 		$$.btnBox.empty();
+		$$.selectedTopic.empty();
 		topics = [];
 	});
 	
@@ -36,29 +42,43 @@ $(document).ready(function(){
 	});
 
 	// Topic Buttons
-	$$.btnBox.on("click", $$.topicBtn, function(){
+	$$.btnBox.on("click", "button", function(){
 		var topicCall = $(this).text().trim();
-		var query = "http://api.giphy.com/v1/gifs/search?q="+topicCall+"&api_key=dc6zaTOxFJmzC&limit=5";
 		
-		$$.selectedTopic.text(topicCall);
+		var query = "http://api.giphy.com/v1/gifs/search?q="+topicCall+"&api_key=dc6zaTOxFJmzC&limit=10";
+		
+		$$.selectedTopic.text(topicCall+" - ");
+		$$.gifBox.empty();
 
 		$.ajax({
 			url: query,
 			method: "GET"
 		}).done(function(response){
-			var gifs = response.data;
+	
+			for(var i = 0; i<response.data.length; i++){	
+				$$.gifBox.append(displayGif(response.data[i]));
+			}
 
-			console.log(gifs);
 		});
 	});
 
+	// Gifs Stop and Go
+	$$.gifBox.on("click", "img", function(){
+		var gifState = $(this).attr("data-state");
 
-
-
-
-
+		if(gifState === "still"){
+			$(this).attr("src",$(this).data("animate"));
+			$(this).attr("data-state", "animate");
+		}else{
+			$(this).attr("src", $(this).data("still"));
+			$(this).attr("data-state", "still");
+		}
+		
+	});
 });
 
+
+/*=========== functions ========================*/
 function validateEntry (arr, searchInput){
 	
 	if (searchInput === ""){
@@ -74,7 +94,7 @@ function validateEntry (arr, searchInput){
 function displayBtn (arr){
 
 	var btn = $("<button>");
-			btn.addClass("topic-btn btn btn-default");
+			btn.addClass("topic-btn btn btn-success btn-info");
 			btn.attr("type","button");
 	
 	for (var i = 0; i<arr.length; i++){
@@ -83,8 +103,16 @@ function displayBtn (arr){
 		$$.btnBox.append(newbtn);
 	}
 }
-function displayGif (arr){
-
+function displayGif (gifObj){
+	var gif = $("<div class='col-sm-3'>");
+			gif.append("<div class='thumbnail-gif thumbnail'>");
+			gif.find(".thumbnail-gif").append("<img class='gif'>");
+			gif.find(".thumbnail-gif").append("<h5 class='text-center'> Rating: "+gifObj.rating+"</h5>");
+			gif.find(".gif").attr("src", gifObj.images.fixed_height_still.url);
+			gif.find(".gif").attr("data-still", gifObj.images.fixed_height_still.url);
+			gif.find(".gif").attr("data-animate", gifObj.images.fixed_width.url);	
+			gif.find(".gif").attr("data-state", "still");
+	return gif;
 }
 
 
